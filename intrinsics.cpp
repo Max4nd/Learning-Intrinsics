cce1f4b2580b30cf73a71fc62aa0acacea16d113
Max Anderson <thing4444@hotmail.com>
2021-05-08 07:13:46 +1000

    added struct for easy value passing

--- intrinsics.cpp
+++ intrinsics.cpp
@@ -2,15 +2,41 @@
 #include <immintrin.h>
 using namespace std;
 
+//Simplifies __m128 operations
+struct fourValues {
+	float a, b, c, d;
+	fourValues(float setA, float setB, float setC, float setD) {
+		a = setA;
+		b = setB;
+		c = setC;
+		d = setD;
+	}
+};
+
+//Simplifies __m256 operations
+struct eightValues {
+	float a, b, c, d, e, f, g, h;
+	eightValues(float setA, float setB, float setC, float setD, float setE, float setF, float setG, float setH) {
+		a = setA;
+		b = setB;
+		c = setC;
+		d = setD;
+		e = setE;
+		f = setF;
+		g = setG;
+		h = setH;
+	}
+};
+
 //Function to add 2 floats 8 times simultaneously
 //This is completly useless and has no real world application whatsoever 
-float* addSimultaneously(float a, float b) {
+float* addSimultaneously(eightValues a, eightValues b) {
 	//Registers
 	__m256 _x, _y, _mask;
 
 	//Set the values of the registers
-	_x = _mm256_set1_ps(a);
-	_y = _mm256_set1_ps(b);
+	_x = _mm256_set_ps(a.a, a.b, a.c, a.d, a.e, a.f, a.g, a.h);
+	_y = _mm256_set_ps(b.a, b.b, b.c, b.d, b.e, b.f, b.g, a.h);
 
 	//Format: __mm<size>_<operation>_<datatype>
 	//This just adds the 2 vectors
@@ -22,9 +48,13 @@ float* addSimultaneously(float a, float b) {
 
 int main() {
 
+	//pass in some values
+	eightValues test1(3.0, 6.0, 8.0, 9.0, 100.0, 12.0, 7.0, 8.0);
+	eightValues test2(3.0, 6.0, 8.0, 9.0, 100.0, 12.0, 7.0, 8.0);
+
 	//print 
 	for (int i = 0; i < 8; i++)
-		cout << addSimultaneously(5, 5)[i] << " ";
+		cout << addSimultaneously(test1, test2)[i] << " ";
 
 	return 0;
 }
